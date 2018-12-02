08c976676ca6ca6d3b7347b117aa5aa04c905076
Maciej Rostanski <mrostanski@gmail.com>
2018-12-02 14:16:58 +0100

    Step 2A

---
+++ terraform/2A_simple_ec2_resource/ec2-sg.tf
@@ -0,0 +1,37 @@
+
+# ## WARNING! - SPECIAL type of resource (adopted, not created)
+# resource "aws_default_vpc" "default" {
+#   tags {
+#     Name = "Default VPC"
+#   }
+# }
+
+# resource "aws_security_group" "ec2-sg" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH"
+#   vpc_id      = "${aws_default_vpc.default.id}"
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"  # let's try to do it "-1" here, compare plan to apply effect!
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"  
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"
+#     cidr_blocks     = ["0.0.0.0/0"]
+#   }
+# }
+
+
+
---
+++ terraform/2A_simple_ec2_resource/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "simple_ec2" {
+   ami             = "ami-046e77060c499c01d"
+
+   instance_type   = "t2.micro"
+   vpc_security_group_ids = ["${aws_security_group.ec2-sg.id}"]
+}
+
+# output "simple_ec2_private_ip" {
+#   value = "${aws_instance.simple_ec2.private_ip}"
+# }
+
+# output "simple_ec2_public_ip" {
+#   value = "${aws_instance.simple_ec2.public_ip}"
+# }
+
+# output "simple_ec2_key_name" {
+#   value = "${aws_instance.simple_ec2.key_name}"
+# }
+
+
+# To add:
+
+#    key_name        = "XXX"
+#
+
+# data "aws_ami" "server_ami" {
+#   most_recent      = true
+
+
+#   filter {
+#     name   = "name"
+#     values = ["my_httpd"]
+#   }
+
+#   owners     = ["self"]
+# }
